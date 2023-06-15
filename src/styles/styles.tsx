8a9bbd569ee676f68b587a79cb96cbec64215f00
Alex <alexsandrolima991@hotmail.com>
2023-06-15 13:18:23 -0300

    chore: create style file

---
+++ src/styles/styles.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Body = styled.body`
+  margin: 0px;
+  padding: 0px;
+`;
+
+export { Title, Body, Container };
